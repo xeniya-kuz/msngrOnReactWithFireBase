cb2d9211856ad960e7229d6bfc2371e386ec6f07
xeniya-kuz <101529480+xeniya-kuz@users.noreply.github.com>
2022-03-25 21:48:05 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { RoutesComponent } from './components/Routes/Routes'
+import { Provider } from "react-redux"
+import { store, persistor } from './store';
+import './App.sass';
+import { PersistGate } from 'redux-persist/integration/react';
+import { CircularProgress } from '@material-ui/core';
+import './fontawesome';
+
+
+function App() {
+
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor} loading={<CircularProgress />}>
+        <RoutesComponent />
+      </PersistGate>
+    </Provider>
+  )
+}
+
+export default App;
